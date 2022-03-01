c77b094479b1c24c788e1314986ea23362d2b917
Sima612 <sima101531@yahoo.com>
2022-02-28 18:27:55 -0800

    styling change

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const containers = document.getElementsByClassName('.container')
+const container = document.querySelector('.container')
+const taken = document.querySelectorAll('.taken')
 const displayCurrentPlayer = document.querySelector('.current-player')
 const results = document.querySelector('.results')
-    
-//Add EventListener
-document.getElementsByClassName('.row').addEventListener('click', index => {
-    let currentPlayer = 1
-    let len = rows.length
-    for(let i =0;i < len; i++) {
-        if(rows[index + 7].classList.contains('taken')) {
-            if(currentPlayer === 1) {
-                rows[index].classList.add('taken', 'player-one')
-            
-                currentPlayer = 2
-                displayCurrentPlayer.innerHTML = currentPlayer
-            } else if(currentPlayer === 2) {
-                rows[index].classList.add('taken', 'player-two')
+const winningArray = [
+    [0, 1, 2, 3],
+    [1, 2, 3, 4],
 
-                currentPlayer = 1
-                displayCurrentPlayer.innerHTML = currentPlayer
-            } 
-        } else alert('Cannot go there')
-    }(i)
-    document.append(rows)
-})
+]
+
+let currentPlayer = 1
+
+function square() {
+    const box = container.length
+    for(let i = 0; i < box; i++) {
+        
+        
+    }
+}
+
 
